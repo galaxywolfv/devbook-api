5c3a72d8286008403634ba7db971670e2de6597d
Devran <devran_ilhan@live.nl>
2023-10-24 21:55:36 +0200

    feat: add additional role author

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -13,12 +13,21 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: Number,
-    enum: [0, 1],
-    default: 1
+    enum: [0, 1, 2],
+    //0 = admin, 1 = author, 2 = user
+    default: 2
   },
-  token: { 
-    type: String 
+  token: {
+    type: String
   },
+  savedBooks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book"
+  }],
+  publishedBooks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book"
+  }]
 });
 
 module.exports = mongoose.model("user", userSchema);
